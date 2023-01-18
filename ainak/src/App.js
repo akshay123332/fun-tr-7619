1b2678cf219c7a827e6f18af88b462f08e49e3ad
akshay kumar <akshay123332@gmail.com>
2023-01-19 00:03:10 +0530

    git init

--- ainak/src/App.js
+++ ainak/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Menspage from './Pages/Menspage';
 
 function App() {
   return (
     <div className="App">
       <h1>Enter</h1>
-      <h1>ju</h1>
+       <Menspage/>
     </div>
   );
 }
