8d58b1943745d929924e709ad79fbbb0a1fcfa29
akarshabhardwaj <akarshabhardwaj7280976503@gmail.com>
2023-01-18 18:45:38 +0530

    day2 work fw21_0435

--- ainak/src/App.js
+++ ainak/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Homepage from './Pages/Homepage';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Enter</h1>
-      <h1>ju</h1>
-    </div>
+      <Homepage/>
+     </div>
   );
 }
 
---
+++ ainak/src/Pages/ComputerGlasses/Computerglasses.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import {SimpleGrid } from '@chakra-ui/react'
+import ProductCard from './ProductCard'
+
+const Computerglasses = () => {
+const[data,setData]=useState([])
+
+const getData=()=>{
+    axios.get("https://rich-gray-scarab-fez.cyclic.app/computerglasses")
+    .then((res)=>setData(res.data))
+    .catch((err)=>console.log(err))
+}
+//style={{display:"grid", gridTemplateColumns:"repeat(3,1fr)" , gap:"50px"}}
+//console.log(data);
+
+useEffect(()=>{
+    getData()
+},[])
+
+  return (
+    <div>
+        <SimpleGrid columns={[1,2,3,3,3]} spacing={25} padding="2%"  >
+            {data?.map((el,index)=>{
+               return <div style={{border:"2px solid grey",padding:"2%" , borderRadius:"7px"}} key={el.id} >
+                <ProductCard id={el.id} image={el.image} desc={el.desc} price={el.price} rating={el.rating} frameshape={el.frameshape} compatible={el.compatible} />
+               </div>
+            })}
+        </SimpleGrid>
+    </div>
+  )
+}
+
+export default Computerglasses
