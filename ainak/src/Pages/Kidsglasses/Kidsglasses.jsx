7c79c7f13c4075b8da17e366a0900a15194f7925
akarshabhardwaj <akarshabhardwaj7280976503@gmail.com>
2023-01-19 19:53:28 +0530

    category

--- ainak/src/Pages/Kidsglasses/Kidsglasses.jsx
+++ ainak/src/Pages/Kidsglasses/Kidsglasses.jsx
@@ -3,30 +3,44 @@ import axios from "axios"
 import {SimpleGrid } from '@chakra-ui/react'
 import ProductCard from './ProductCard'
 import styles from "./kidsglass.module.css"
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import { useDispatch, useSelector } from 'react-redux'
+import { useLocation, useSearchParams } from 'react-router-dom'
+import { getData } from '../../redux/menspage/action'
 
 const Kidsglasses = () => {
-const[data,setData]=useState([])
 
-const getData=()=>{
-    axios.get("https://rich-gray-scarab-fez.cyclic.app/kidsglasses")
-    .then((res)=>setData(res.data))
-    .catch((err)=>console.log(err))
-}
-//style={{display:"grid", gridTemplateColumns:"repeat(3,1fr)" , gap:"50px"}}
-//console.log(data);
+let kids_url="https://rich-gray-scarab-fez.cyclic.app/eyeglasses"
+
+const dispatch=useDispatch()
+const data=useSelector((store)=>store.data);
+const location=useLocation();
+const [searchparams]=useSearchParams()
+console.log(data)
 
 useEffect(()=>{
-    getData()
-},[])
+    let paramObj={
+        params:{
+            compatible:searchparams.getAll(`compatible`),
+            frameshape:searchparams.getAll(`frameshape`)
+        },
+    }
+    dispatch(getData(kids_url,paramObj))
+},[location.search])
+
 
   return (
     <div>
-        <div columns={[1,2,3,3,3]} spacing={25} padding="2%" className={`${styles.product}`} >
-            {data?.map((el,index)=>{
+         <div style={{display:"flex"}}>
+          <div style={{width:"20%"}}><Sidebar/></div>
+          <div columns={[1,2,3,3,3]} spacing={25} padding="2%" className={`${styles.product}`}>
+          {data?.map((el,index)=>{
                return <div style={{border:"thick double #32a1ce",padding:"2%" , borderRadius:"7px" , backgroundColor:"azure", boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px"}} key={el.id} >
                <ProductCard id={el.id} image={el.image} desc={el.desc} price={el.price} rating={el.rating} frameshape={el.frameshape} compatible={el.compatible} />
                </div>
             })}
+          </div>
+            
         </div>
     </div>
   )
